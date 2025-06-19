f2d4b7ef3226a87ba4927a9e294d7d4ac012eb6b
Emazov <emir.almazovich@gmail.com>
2025-06-19 20:05:34 +0600

    fix: Start Page

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,37 +1,19 @@
-import { useEffect } from 'react';
-import { useTelegram } from '../hooks/useTelegram';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const StartPage = () => {
-	const { tg } = useTelegram();
-	const navigate = useNavigate();
-
-	useEffect(() => {
-		tg.MainButton.setText('Поехали!');
-		tg.MainButton.show();
-
-		const onMainButtonClick = () => {
-			navigate('/guide');
-		};
-
-		tg.MainButton.onClick(onMainButtonClick);
-
-		return () => {
-			tg.MainButton.offClick(onMainButtonClick);
-			tg.MainButton.hide();
-		};
-	}, [tg, navigate]);
-
 	return (
 		<div className='welcome bg-[url("/main_bg.jpg")] bg-cover bg-center h-full'>
-			<div className='container flex flex-col justify-around h-full'>
-				<div className='hero flex flex-col items-center'>
-					<h2 className='text-white text-[clamp(2.5rem,9vw,3.5rem)] text-center font-bold'>
-						Добро пожаловать!
-					</h2>
-
-					<img src='./main_logo.png' alt='logo' className='logo' />
-				</div>
+			<div className='container flex flex-col justify-between h-full'>
+				<h2 className='text-white text-[clamp(2.5rem,9vw,3.5rem)] text-center font-bold'>
+					Добро пожаловать!
+				</h2>
+				<img src='./main_logo.png' alt='logo' className='logo' />
+				<Link
+					to='/guide'
+					className='link_btn bg-[#FFEC13] text-[clamp(1rem,2vh,1.5rem)] text-black py-[clamp(1rem,1vh,2rem)]'
+				>
+					Поехали!
+				</Link>
 			</div>
 		</div>
 	);
