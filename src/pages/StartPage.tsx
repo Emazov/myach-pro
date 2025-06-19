bc89e1d6e5f3cb26141e6dd1f7a034d842deca83
Emazov <emir.almazovich@gmail.com>
2025-06-19 20:12:42 +0600

    fix: Start Page container

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -3,11 +3,14 @@ import { Link } from 'react-router-dom';
 const StartPage = () => {
 	return (
 		<div className='welcome bg-[url("/main_bg.jpg")] bg-cover bg-center h-full'>
-			<div className='container flex flex-col justify-between h-full'>
-				<h2 className='text-white text-[clamp(2.5rem,9vw,3.5rem)] text-center font-bold'>
-					Добро пожаловать!
-				</h2>
-				<img src='./main_logo.png' alt='logo' className='logo' />
+			<div className='container flex flex-col justify-around h-full'>
+				<div className='hero flex flex-col items-center'>
+					<h2 className='text-white text-[clamp(2.5rem,9vw,3.5rem)] text-center font-bold'>
+						Добро <br />
+						пожаловать!
+					</h2>
+					<img src='./main_logo.png' alt='logo' className='logo' />
+				</div>
 				<Link
 					to='/guide'
 					className='link_btn bg-[#FFEC13] text-[clamp(1rem,2vh,1.5rem)] text-black py-[clamp(1rem,1vh,2rem)]'
