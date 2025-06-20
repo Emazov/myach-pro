1d9c4ede20582c1912921ba5c525c3fad1fc00ee
Emazov <emir.almazovich@gmail.com>
2025-06-20 09:44:23 +0600

    init

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,17 +4,23 @@ const Modal = ({ isOpen, message, onClose }: ModalProps) => {
 	if (!isOpen) return null;
 
 	return (
-		<div className='fixed inset-0 flex items-center justify-center z-50 bg-black/50'>
-			<div className='bg-white rounded-lg p-6 w-[80%] max-w-md'>
+		<div className='fixed inset-0 flex items-end z-50 bg-black/50'>
+			<div className='bg-white rounded-lg p-6 w-full'>
 				<p className='text-center mb-6 text-[clamp(1rem,4vw,1.5rem)]'>
 					{message}
 				</p>
-				<div className='flex justify-center'>
+				<div className='flex flex-col gap-2 mb-[clamp(1rem,1.5vh,1rem)]'>
 					<button
 						onClick={onClose}
-						className='bg-[#EC3381] text-white py-2 px-8 rounded-lg text-[clamp(1rem,3vw,1.2rem)]'
+						className='link_btn bg-[#EC3381] border-1 border-[#EC3381] text-white py-[clamp(1rem,1vw,1rem)] text-[clamp(1rem,2vh,1.5rem)]]'
 					>
-						OK
+						Заменить игрока
+					</button>
+					<button
+						onClick={onClose}
+						className='link_btn border-1 border-[#EC3381] text-[#EC3381] py-[clamp(1rem,1vw,1rem)] text-[clamp(1rem,2vh,1.5rem)]'
+					>
+						Выбрать другую категорию
 					</button>
 				</div>
 			</div>
